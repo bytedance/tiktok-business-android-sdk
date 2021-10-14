80a5d45f3b1bdb8ca780e94bf3f103fb9d3cb19e
yuzhong-qian <yuzhong.qian.cn@gmail.com>
2021-10-13 19:46:27 -0700

    feat: support 307 redirect (#19)
    Co-authored-by: yuzhong.qian <yuzhong.qian@bytedance.com>

--- business-core/src/main/java/com/tiktok/util/HttpRequestUtil.java
+++ business-core/src/main/java/com/tiktok/util/HttpRequestUtil.java
@@ -47,30 +47,69 @@ public class HttpRequestUtil {
         return doGet(url, headerParamMap, options);
     }
 
-    public static String doGet(String url, Map<String, String> headerParamMap, HttpRequestOptions options) {
-        String result = null;
-
+    public static HttpsURLConnection connect(String url, Map<String, String> headerParamMap, HttpRequestOptions options, String method, String contentLength) {
         HttpsURLConnection connection = null;
 
         try {
             URL httpURL = new URL(url);
             connection = (HttpsURLConnection) httpURL.openConnection();
-            connection.setRequestMethod("GET");
+            connection.setRequestMethod(method);
             options.configConnection(connection);
-            connection.setDoOutput(false);
             connection.setDoInput(true);
             connection.setUseCaches(false);
+            if(method.equals("GET")) {
+                connection.setDoOutput(false);
+            } else if(method.equals("POST")) {
+                connection.setDoOutput(true);
+                connection.setRequestProperty("Content-Length", contentLength);
+            }
 
             for (Map.Entry<String, String> entry : headerParamMap.entrySet()) {
                 connection.setRequestProperty(entry.getKey(), entry.getValue());
             }
 
             connection.connect();
+        } catch (Exception e) {
+            TTCrashHandler.handleCrash(TAG, e);
+            if (connection != null) {
+                try {
+                    connection.disconnect();
+                }catch (Exception exc){
+                    TTCrashHandler.handleCrash(TAG, exc);
+                }
+            }
+        }
+        return connection;
+    }
+
+    public static boolean shouldRedirect(int status) {
+        if (status != HttpURLConnection.HTTP_OK) {
+            if (status == HttpURLConnection.HTTP_MOVED_TEMP
+                    || status == HttpURLConnection.HTTP_MOVED_PERM
+                    || status == HttpURLConnection.HTTP_SEE_OTHER
+                    || status == 307)
+                return true;
+        }
+        return false;
+    }
+
+    public static String doGet(String url, Map<String, String> headerParamMap, HttpRequestOptions options) {
+        String result = null;
+        HttpsURLConnection connection = connect(url, headerParamMap, options, "GET", null);
+        if (connection == null) return result;
+
+        try{
+            boolean redirect = shouldRedirect(connection.getResponseCode());
+            if (redirect) {
+                String redirectUrl = connection.getHeaderField("Location");
+                connection.disconnect();
+                connection = connect(redirectUrl, headerParamMap, options, "GET", null);
+            }
+
             int responseCode = connection.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 result = streamToString(connection.getInputStream());
             }
-
         } catch (Exception e) {
             TTCrashHandler.handleCrash(TAG, e);
         } finally {
@@ -82,7 +121,6 @@ public class HttpRequestUtil {
                 }
             }
         }
-
         return result;
     }
 
@@ -103,25 +141,22 @@ public class HttpRequestUtil {
 
         try {
             byte[] writeBytes = jsonStr.getBytes("UTF-8");
+            String contentLength = String.valueOf(writeBytes.length);
 
-            URL httpURL = new URL(url);
-
-            connection = (HttpURLConnection) httpURL.openConnection();
-            connection.setRequestMethod("POST");
-            options.configConnection(connection);
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setUseCaches(false);
-
-            for (Map.Entry<String, String> entry : headerParamMap.entrySet()) {
-                connection.setRequestProperty(entry.getKey(), entry.getValue());
-            }
-            connection.setRequestProperty("Content-Length", String.valueOf(writeBytes.length));
-
-            connection.connect();
+            connection = connect(url, headerParamMap, options, "POST", contentLength);
+            if (connection == null) return result;
             outputStream = connection.getOutputStream();
             outputStream.write(writeBytes);
             outputStream.flush();
+            boolean redirect = shouldRedirect(connection.getResponseCode());
+            if (redirect) {
+                String redirectUrl = connection.getHeaderField("Location");
+                connection.disconnect();
+                connection = connect(redirectUrl, headerParamMap, options, "POST", contentLength);
+                outputStream = connection.getOutputStream();
+                outputStream.write(writeBytes);
+                outputStream.flush();
+            }
 
             int responseCode = connection.getResponseCode();
             // http code is different from the code returned by api
