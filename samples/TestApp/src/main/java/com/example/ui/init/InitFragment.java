43dc38da15e87f146e7f2b2f6c9c87e70ac71507
王超峰 <wangchaofeng.fengny@bytedance.com>
2023-11-27 16:19:02 +0800

    feature:android sdk supports automatic IAP tracking&Standardized Event Reporting.

--- samples/TestApp/src/main/java/com/example/ui/init/InitFragment.java
+++ samples/TestApp/src/main/java/com/example/ui/init/InitFragment.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020. Bytedance Inc.
+ * Copyright (c) 2023. Bytedance Inc.
  *
  * This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
  ******************************************************************************/
